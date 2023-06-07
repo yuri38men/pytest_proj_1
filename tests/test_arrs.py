73cc4488c2ea3afe91a452fb0cbeecb512cfa2cd
User <yuri.ogorodnik@mail.ru>
2023-06-07 21:28:15 +0300

    test adjustment

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,15 @@
+import pytest
+
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+
+
+def test_get_index_error():
+    with pytest.raises(IndexError):
+        arrs.get([], 0, "test")
 
 
 def test_slice():
